be4ebf0416c2685fa764c98368e83fb8fcedb8ea
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-28 03:48:06 +0800

    reverse_integer refactor

--- reverse_integer/reverse_integer.rb
+++ reverse_integer/reverse_integer.rb
@@ -1,8 +1,6 @@
 def reverse_integer(int)
-  return 0 if int > 2147483647 || int < -2147483647 # 超出範圍回傳 0
-  string = int.to_s         # 將整數轉換成字串
-  string.reverse!           # 將字串反轉
-  result = string.to_i      # 將字串轉換成數字，存入 result
-  result *= -1 if int < 0   # 如果原來的整數是負數，result 變成負數
+  return 0 if int > 2147483647 || int < -2147483647
+  result = int.to_s.reverse!.to_i     # 整數轉換成字串，反轉後再變回整數
+  result *= -1 if int < 0
   return result
 end
