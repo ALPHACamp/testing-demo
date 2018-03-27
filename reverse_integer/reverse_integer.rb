61c675939b01bbcc900e92ca8da9838d7573882c
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-28 03:44:56 +0800

    reverse_integer v2

--- reverse_integer/reverse_integer.rb
+++ reverse_integer/reverse_integer.rb
@@ -1,5 +1,7 @@
 def reverse_integer(int)
   string = int.to_s     # 將整數轉換成字串
   string.reverse!       # 將字串反轉
-  return string.to_i    # 將字串轉換成數字，並回傳
+  result =  string.to_i    # 將字串轉換成數字，並回傳
+  result *= -1 if int < 0   # 如果原來的整數是負數，result 變成負數
+  return result
 end
