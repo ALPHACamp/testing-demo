0130e576778524b6dccf67b7ba0f5f8f09064a78
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-28 20:27:38 +0800

    multi combo all need discount

--- potter-kata/lib/cart.rb
+++ potter-kata/lib/cart.rb
@@ -10,10 +10,15 @@ class Cart
     total = 0
     @ordered_items = @ordered_items.delete_if{ |i| i == 0 } # 刪除客戶沒有買的哈利波特集數
 
-    if @ordered_items.size == 2       # 如果買了一組，要依原價打 5% 折扣
-      total = total + 2 * PRICE * 0.95
+    if @ordered_items.size == 2       # 如果客戶買了兩集的哈利波特
+      while @ordered_items.size > 0   # 當還有書籍時
+        total = total + 2 * PRICE * 0.95   # 把客戶買的一組哈利波特打折後計入總價
+        @ordered_items[0] -= 1       # 刪除已計算過的書籍
+        @ordered_items[1] -= 1       # 刪除已計算過的書籍
+        @ordered_items = @ordered_items.delete_if{ |i| i == 0 }
+      end
     else
-      for item in @ordered_items do   # 如果只買了單集 1 本或多本，就原價計算
+      for item in @ordered_items do   # 客戶只買了單一集的哈利波特
         total = total + item * PRICE
       end
     end
