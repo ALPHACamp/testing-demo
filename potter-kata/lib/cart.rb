56c72b5e6df7226c3ffa62f38ad1487fc54f5739
Ellen Lee <leechinghui.tw@gmail.com>
2018-03-28 20:23:16 +0800

    only 1 potter combo and need discount

--- potter-kata/lib/cart.rb
+++ potter-kata/lib/cart.rb
@@ -8,9 +8,16 @@ class Cart
 
   def calculate    # 計算客戶所買的書本總價
     total = 0
-    for item in @ordered_items do         # 把書拿出來計算
-      total = total + item * PRICE
+    @ordered_items = @ordered_items.delete_if{ |i| i == 0 } # 刪除客戶沒有買的哈利波特集數
+
+    if @ordered_items.size == 2       # 如果買了一組，要依原價打 5% 折扣
+      total = total + 2 * PRICE * 0.95
+    else
+      for item in @ordered_items do   # 如果只買了單集 1 本或多本，就原價計算
+        total = total + item * PRICE
+      end
     end
+
     return total
   end
 
